c19d81dc5b407f21b0c3d090d8332db401e2dd56
c1m50c <terminalityms@gmail.com>
2021-11-16 16:46:47 -0600

    Cell `__setattr__`, `__eq_-`, `__ne__` ✔

--- src/celll.py
+++ src/celll.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from __future__ import annotations
+from typing import Tuple, Any
 
 
 class Cell:
@@ -8,21 +9,42 @@ class Cell:
         ## Fields
         ```py
         position: Tuple[int, int] = (0, 0) # Position of the Cell within the grid.
+        material_id: int = 0 # Cell's Material defaults to none.
         ```
     """
     
     position: Tuple[int, int]
+    material_id: int
     
-    __slots__ = "position"
+    __slots__ = "position", "material_id"
     
-    def __init__(self, position: Tuple[int, int] = (0, 0)) -> None:
+    def __init__(self, position: Tuple[int, int] = (0, 0), material_id: int = 0) -> None:
         """
             Cell class for simulating cellular automata.
             
             ## Parameters
             ```py
             position: Tuple[int, int] = (0, 0) # Position of the Cell within the grid.
+            material_id: int = 0 # Cell's Material defaults to none.
             ```
         """
         
         self.position = position
+        self.material_id = material_id
+    
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == "material_id" and isinstance(value, int):
+            if value >= 0:
+                self.material_id = value
+            else:
+                raise AttributeError(f"Cannot set attribute {name} to a non-positive integer.")
+        elif name == "position" and isinstance(value, Tuple[int, int]):
+            self.position = value
+        else:
+            raise AttributeError(f"Attribute {name} was either not found or of invalid type.")
+    
+    def __eq__(self, other: Cell) -> bool:
+        return self.material_id == other.material_id
+
+    def __ne__(self, other: object) -> bool:
+        return self.material_id != other.material_id
