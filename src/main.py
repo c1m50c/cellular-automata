f020d4e7b0ae1d75c339e033e52ee5948e4dc41f
c1m50c <terminalityms@gmail.com>
2021-11-16 17:49:21 -0600

    Better Event handling ✋

--- src/main.py
+++ src/main.py
@@ -57,12 +57,15 @@ def main():
         selected_rect = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
         pygame.draw.rect(SCREEN, SELECTED_CELL_COLOR, selected_rect, 2)
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            return
+        
+        if not simulating:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1: # Left Mouse Button Down
                 print(f"Placing Cell @ {x, y}")
                 cells.add(Cell(position=(x, y)))
+        
         pygame.display.flip()
 
 
