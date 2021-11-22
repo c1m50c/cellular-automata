a6049b01aa5b1f2f760fe853d4c3432a79d877f3
c1m50c <terminalityms@gmail.com>
2021-11-21 19:36:15 -0600

    Window Resizing ⚖

--- src/main.py
+++ src/main.py
@@ -11,20 +11,20 @@ CELL_COLOR: Tuple[int, int, int] = (222, 186, 80)
 SELECTED_CELL_COLOR: Tuple[int, int, int] = (248, 248, 248)
 
 # Misc Settings #
-WIDTH, HEIGHT = 1280, 1024
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+CELL_SIZE: int = 24
+SCREEN = pygame.display.set_mode((800, 512), pygame.RESIZABLE)
 CLOCK = pygame.time.Clock()
-CELL_SIZE: int = 32
 
 # Simulation Variables #
-iterations: int = 0
-show_grid: bool = True
 cells: CellContainer[Cell] = CellContainer()
+width, height = SCREEN.get_size()
+show_grid: bool = True
 
 
 def draw_grid():
-    for x in range(0, WIDTH, CELL_SIZE):
-        for y in range(0, HEIGHT, CELL_SIZE):
+    global width, height
+    for x in range(0, width, CELL_SIZE):
+        for y in range(0, height, CELL_SIZE):
             rect_cell = pygame.Rect(x, y, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(SCREEN, GRID_COLOR, rect_cell, 1)
 
@@ -46,32 +46,34 @@ def get_cell_at_mouse_pos() -> Tuple[int, int]:
 
 
 def simulate():
-    global iterations
+    global width, height
     global cells
     
     new_cells = CellContainer()
-    for x in range(0, WIDTH, CELL_SIZE):
-        for y in range(0, HEIGHT, CELL_SIZE):
-            neighbors = cells.get_neighbors((x, y), CELL_SIZE, (WIDTH, HEIGHT))
+    for x in range(0, width, CELL_SIZE):
+        for y in range(0, height, CELL_SIZE):
+            neighbors = cells.get_neighbors((x, y), CELL_SIZE, (width, height))
             n: int = len(neighbors)
             cell: Cell = cells.get_cell_at_position((x, y))
             
             if cell:
                 if n == 2 or n == 3:
-                    new_cells.add(Cell(position=(x, y)), extents=(WIDTH, HEIGHT))
+                    new_cells.add(Cell(position=(x, y)), extents=(width, height))
             elif n == 3 and not cell: # Cell Revives
-                new_cells.add(Cell(position=(x, y)), extents=(WIDTH, HEIGHT))
+                new_cells.add(Cell(position=(x, y)), extents=(width, height))
     
     cells = new_cells
-    iterations += 1
 
 
 def main():
+    global width, height
     global show_grid
+    
     pygame.display.set_caption("Cellular Automata")
     simulating: bool = False
     
     while True:
+        width, height = SCREEN.get_size()
         SCREEN.fill(BACKGROUND_COLOR)
         draw_cells()
         
@@ -95,8 +97,7 @@ def main():
         
         if not simulating and event.type == pygame.MOUSEBUTTONDOWN :
             if event.button == 1: # Left Mouse Button Down
-                if x >= 0 and y >= 0 and x <= WIDTH and y <= HEIGHT:
-                    cells.add(Cell(position=(x, y)), extents=(WIDTH, HEIGHT))
+                cells.add(Cell(position=(x, y)), extents=(width, height))
             elif event.button == 3: # Right Mouse Button Down
                 cell = cells.get_cell_at_position((x, y))
                 if cell:
